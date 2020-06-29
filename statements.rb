d751d60e1a805dab91f297ed81ca7b1ddf793193
Sara McCombs <thesaramccombs@gmail.com>
2020-06-29 14:20:12 +0000

    Automatically backed up by Learn

---
+++ statements.rb
@@ -0,0 +1,16 @@
+letter = "a"
+def vowels_with_if_elsif(letter)
+  if letter == "a"
+    true
+  elsif letter == "e"
+    true
+  elsif letter == "i"
+    true
+  elsif letter == "o"
+    true
+  elsif letter == "u"
+    true
+  else
+    false
+  end
+end
